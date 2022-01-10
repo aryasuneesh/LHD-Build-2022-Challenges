c27495ff8feacd731686226b85c5c4fd6b11a0b8
aryasuneesh <aryasuneesh3@gmail.com>
2022-01-10 15:32:39 +0400

    Final changes

--- articleanalysis/articleanalysis.py
+++ articleanalysis/articleanalysis.py
@@ -2,6 +2,7 @@ pos = open("articleanalysis/positive-words.txt","r")
 poslist=[]
 for l1 in pos:
     poslist.append(l1.split())
+
 neglist=[]
 neg = open("articleanalysis/negative-words.txt","r")
 for l2 in neg:
@@ -17,29 +18,36 @@ def apostrophecheck(i):
     elif i[-1]=="'" or i[-1]=="\"":
         i = i[:-1]
         return i
-    
-artclTitle = input("Enter article title:")
-artclList = artclTitle.split(" ")
-posCount, negCount = 0, 0
-for i in artclList:
-    k = apostrophecheck(i)
-    for j in poslist:
-        if k==j[0]:
-            posCount+=1
 
-for i in artclList:
-    k = apostrophecheck(i)
-    for j in neglist:
-        if k==j[0]:
-            negCount+=1
+p = 'y'
+while p=='y' or p=='Y':
+    print(" ======ArticleAnalyser======")
+    artclTitle = input("    Enter article title:")
+    artclList = artclTitle.split(" ")
+    posCount, negCount = 0, 0
+    for i in artclList:
+        k = apostrophecheck(i)
+        for j in poslist:
+            if k==j[0]:
+                posCount+=1
 
-if posCount>negCount:
-    print("The article is POSITIVE")
-elif posCount==negCount:
-    print("The article is NEUTRAL")
-elif posCount==0 or negCount==0:
-    print("Unable to compute sentiment")
-else:
-    print("The article is NEGATIVE")
+    for i in artclList:
+        k = apostrophecheck(i)
+        for j in neglist:
+            if k==j[0]:
+                negCount+=1
+
+    if posCount>negCount:
+        print(" The article is POSITIVE")
+        p = input(" Would you like to check another article? (y/n): ")
+    elif posCount==negCount:
+        print(" The article is NEUTRAL")
+        p = input(" Would you like to check another article? (y/n): ")
+    elif posCount==0 or negCount==0:
+        print(" Unable to compute sentiment")
+        p = input(" Would you like to check another article? (y/n): ")
+    else:
+        print(" The article is NEGATIVE")
+        p = input(" Would you like to check another article? (y/n): ")
 
 
