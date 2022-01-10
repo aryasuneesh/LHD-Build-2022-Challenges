8ef01114d8318ebbb1183658bd25178a8e59fa33
aryasuneesh <aryasuneesh3@gmail.com>
2022-01-10 15:13:29 +0400

    Final changes

--- articleanalysis/articleanalysis.py
+++ articleanalysis/articleanalysis.py
@@ -10,25 +10,27 @@ for l2 in neg:
 def apostrophecheck(i):
     if i[0]=="'" and i[-1]=="'":
         i = i[1:-1]
+        return i
     elif i[0]=="'" or i[0]=="\"":
         i = i[1:]
+        return i
     elif i[-1]=="'" or i[-1]=="\"":
         i = i[:-1]
+        return i
     
 artclTitle = input("Enter article title:")
 artclList = artclTitle.split(" ")
 posCount, negCount = 0, 0
 for i in artclList:
-
+    k = apostrophecheck(i)
     for j in poslist:
-        if i==j[0]:
+        if k==j[0]:
             posCount+=1
 
 for i in artclList:
-    if i[0]=="'" and i[-1]=="'":
-        i = i[1:-1]
+    k = apostrophecheck(i)
     for j in neglist:
-        if i==j[0]:
+        if k==j[0]:
             negCount+=1
 
 if posCount>negCount:
