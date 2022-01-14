a6dc77f89fb24731a2446fca7d18fad0c22fafe6
aryasuneesh <aryasuneesh3@gmail.com>
2022-01-14 22:03:35 +0400

    pi

--- pi/pi.py
+++ pi/pi.py
@@ -1,5 +1,6 @@
 import mpmath as mp
-
+# pi
 print("\n=======================PI=======================\n")
 mp.mp.dps = int(input("Enter number of digits of pi to display: "))
 print("Pi: ", mp.mp.pi, "\n")
+# pi
